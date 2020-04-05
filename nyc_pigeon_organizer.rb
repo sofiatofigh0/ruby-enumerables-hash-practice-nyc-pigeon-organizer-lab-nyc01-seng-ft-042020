1ff1b35fa12ab40d6568d0697d1b06dad3ff016f
sofia tofigh <sst2140@columbia.edu>
2020-04-05 18:58:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,7 +9,7 @@ def nyc_pigeon_organizer(data)
         if pigeon_list[name][color_gender_lives] == nil
           pigeon_list[name][color_gender_lives] = []
         end
-        pigeon_list[name][color_gender_lives].push(stats.to_s)
+        pigeon_list[name][color_gender_lives] << (stats.to_s)
       end
     end 
   end
